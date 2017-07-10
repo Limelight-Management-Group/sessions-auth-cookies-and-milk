e36e4048702947ae27a8dccecc577a6b3397a628
waynebanks <thestorerun@yahoo.com>
2017-07-10 13:41:18 -0700

    added googleMapsMarker, this will track my location on a 2 second time-out interval. This is just what I do for fun

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var session = require( 'express-session' );
 const bodyParser = require( 'body-parser' );
 const ejs = require( 'ejs' );
 require('./controllers/userController')(app);
-require('./public/js/tracker')(app);
+require('./views/tracker')(app);
 require('./views/header/head')(app);
 var morgan = require('morgan')
 const path = require("path");
-
-var cookieParser = require('cookie-parser')
+const locationMap = new Map()
+const cookieParser = require('cookie-parser')
 
 //use the morgan middleware to log each transaction
 app.use(morgan('dev'));
@@ -46,6 +46,42 @@ app.get('/', (req, res) => {
     // console.log('session check!', req.session)
 
     res.render('index');
+})
+
+app.get('/tracker', (req, res) => {
+    console.log('here is the tracker')
+    // console.log('session check!', req.session)
+
+    res.render('tracker');
+})
+
+app.get('/viewer', (req, res) => {
+    console.log('this is viewer!')
+    // console.log('session check!', req.session)
+
+    res.render('viewer');
+})
+
+io.on('connection', socket => {
+	socket.on('registerTracker', () => {
+		locationMap.set(socket.id, {lat: null, lng: null})
+	})
+
+	socket.on('updateLocation', pos => {
+		if(locationMap.has(socket.id)) {
+			locationMap.set(socket.id, pos)
+			console.log(socket.id, pos)
+		}
+		// locationMap.set(socket.id)
+	})
+
+	socket.on('requestLocations', () => {
+		socket.emit('locationsUpdate', Array.from(locationMap))
+	})
+
+	socket.on('disconnect', () => {
+		locationMap.delete(socket.id)
+	})
 })
 
 var port = process.env.PORT || 3002
--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,18 +1,25 @@
 
-
+// console.log('this is linked together automatically!!')
 document.addEventListener('DOMContentLoaded', () => {
 	const socket = io('/')
-
-
+	socket.emit('registerTracker')
 
 	const positionOptions = {
 		enableHighAccuracy: true,
 		maximumAge: 0
 	}
 
-	navigator.getlocation.getCurrentPosition(pos => {
-		console.log(pos.coords)
+	setInterval(() => {
+		console.log('tick')
+		navigator.geolocation.getCurrentPosition(pos => {
+		//this is destructuring the object!! very cool shit. I am assigning to lat/lng
+		const { latitude: lat, longitude: lng } = pos.coords
+		socket.emit('updateLocation', { lat, lng })
+
 	}, err => {
 		console.error(err)
 	}, positionOptions)
+
+	}, 2000)
+
 })
